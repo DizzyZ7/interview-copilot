2d37205de0369eca91fb7284fe1ef200a47896d0
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-02-09 03:45:05 +0300

    Create metrics.go

---
+++ backend/internal/metrics/metrics.go
@@ -0,0 +1,17 @@
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	HttpRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total HTTP requests",
+		},
+		[]string{"method", "path", "status"},
+	)
+)
+
+func Init() {
+	prometheus.MustRegister(HttpRequests)
+}
